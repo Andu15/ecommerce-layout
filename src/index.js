f03c4e24ddc35dafea104b9429e477f9e8bd42c0
Andu15 <estefania_8_3@hotmail.com>
2022-03-03 11:39:37 -0500

    style: add css nav

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ const nav = document.querySelector("#btn-toggle");
 const items = document.querySelectorAll(".item");
 const cartBtn = document.querySelector("#cart-btn");
 
+// activa el navbar
 nav.addEventListener("click", function (){
   for(var i = 0; i < items.length; i++)
     items[i].classList.toggle("active")
 });
+
+//activa el modal
